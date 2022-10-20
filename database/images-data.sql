3e2c755cae4197613b41352f20248aaf3cd58359
ju14ik <julijacelinska@yahoo.co.uk>
2022-10-20 10:54:59 +0100

    Resizing images

--- database/images-data.sql
+++ database/images-data.sql
@@ -15,10 +15,10 @@ VALUES
 ('icons/drinks.png','Drinks','content.php?pagename=drinks','image_alt','4',NULL),
 -- HOME PAGE CONTENT --
 -- Thumbnails --
-('main/best-foods.jpg','','','image_alt','2','2'),
-('main/right-calories.jpg','','','image_alt','2','3'),
-('main/right-nutrition.jpg','','','image_alt','2','4'),
-('main/how-often-to-eat.jpg','','','image_alt','2','5'),
+('main/best-foods-th.jpg','','','image_alt','2','2'),
+('main/right-calories-th.jpg','','','image_alt','2','3'),
+('main/right-nutrition-th.jpg','','','image_alt','2','4'),
+('main/how-often-to-eat-th.jpg','','','image_alt','2','5'),
 -- Full size images --
 ('main/best-foods.jpg','','','image_alt','3','2'),
 ('main/right-calories.jpg','','','image_alt','3','3'),
@@ -26,44 +26,44 @@ VALUES
 ('main/how-often-to-eat.jpg','','','image_alt','3','5'),
 -- BREAKFAST --
 -- Thumbnails --
-('breakfast/avocado-toast.jpg','','','image_alt','2','6'),
-('breakfast/oatmeal.jpg','','','image_alt','2','7'),
+('breakfast/avocado-toast-th.jpg','','','image_alt','2','6'),
+('breakfast/oatmeal-th.jpg','','','image_alt','2','7'),
 -- Full size images --
 ('breakfast/avocado-toast.jpg','','','image_alt','3','6'),
 ('breakfast/oatmeal.jpg','','','image_alt','3','7'),
 -- SNACKS --
 -- Thumbnails --
-('snacks/chocolate-cherry-trail-mix.jpg','','','image_alt','2','8'),
-('snacks/cheese-and-meat-platter.jpg','','','image_alt','2','9'),
+('snacks/chocolate-cherry-trail-mix-th.jpg','','','image_alt','2','8'),
+('snacks/cheese-and-meat-platter-th.jpg','','','image_alt','2','9'),
 -- Full size images --
 ('snacks/chocolate-cherry-trail-mix.jpg','','','image_alt','3','8'),
 ('snacks/cheese-and-meat-platter.jpg','','','image_alt','3','9'),
 -- DESSERT --
 -- Thumbnails --
-('desserts/chia-pudding.jpg','','','image_alt','2','10'),
-('desserts/peanut-butter-energy-balls.jpg','','','image_alt','2','11'),
-('desserts/chocolate-protein-cookies.jpg','','','image_alt','2','12'),
+('desserts/chia-pudding-th.jpg','','','image_alt','2','10'),
+('desserts/peanut-butter-energy-balls-th.jpg','','','image_alt','2','11'),
+('desserts/chocolate-protein-cookies-th.jpg','','','image_alt','2','12'),
 -- Full size images --
 ('desserts/chia-pudding.jpg','','','image_alt','3','10'),
 ('desserts/peanut-butter-energy-balls.jpg','','','image_alt','3','11'),
 ('desserts/chocolate-protein-cookies.jpg','','','image_alt','3','12'),
 -- DRINKS --
 -- Thumbnails --
-('drinks/cold-coffee.jpg','','','image_alt','2','13'),
-('drinks/strawberry-smoothie.jpg','','','image_alt','2','14'),
-('drinks/cold-brew-tea.jpg','','','image_alt','2','15'),
+('drinks/cold-coffee-th.jpg','','','image_alt','2','13'),
+('drinks/strawberry-smoothie-th.jpg','','','image_alt','2','14'),
+('drinks/cold-brew-tea-th.jpg','','','image_alt','2','15'),
 -- Full size images --
 ('drinks/cold-coffee.jpg','','','image_alt','3','13'),
 ('drinks/strawberry-smoothie.jpg','','','image_alt','3','14'),
 ('drinks/cold-brew-tea.jpg','','','image_alt','3','15'),
 -- DINNER --
 -- Thumbnails --
-('dinner/guacamole-bruschetta.jpg','','','image_alt','2','16'),
+('dinner/guacamole-bruschetta-th.jpg','','','image_alt','2','16'),
 -- Full size images --
 ('dinner/guacamole-bruschetta.jpg','','','image_alt','3','16'),
 -- LUNCH --
 -- Thumbnails --
-('lunch/roast-beef-sandwich.jpg','','','image_alt','2','17'),
+('lunch/roast-beef-sandwich-th.jpg','','','image_alt','2','17'),
 -- Full size images --
 ('lunch/roast-beef-sandwich.jpg','','','image_alt','3','17');
 
