7453394cd316793d25dd703fcd01e807f0bd7fba
ju14ik <julijacelinska@yahoo.co.uk>
2022-10-24 01:28:50 +0100

    Link fix

--- footer.php
+++ footer.php
@@ -2,19 +2,19 @@
             <ul class="social">
                 <li class="icon facebook">
                     <span class="tooltip">Facebook</span>
-                    <a href="https://www.facebook.com/hikefoodie"><span><i class="fab fa-facebook-f"></i></span></a>
+                    <a href="https://www.facebook.com/hikefoodie"><span><i class="fab fa-facebook-f"></i></span> </a>
                 </li>
                 <li class="icon twitter">
                     <span class="tooltip">Twitter</span>
-                    <a href="https://twitter.com/hikefoodie"><span><i class="fab fa-twitter"></i></span></a>
+                    <a href="https://twitter.com/hikefoodie"><span><i class="fab fa-twitter"></i></span> </a>
                 </li>
                 <li class="icon instagram">
                     <span class="tooltip">Instagram</span>
-                    <a href="https://instagram.com/hike.foodie"><span><i class="fab fa-instagram"></i></span></a>
+                    <a href="https://instagram.com/hike.foodie"><span><i class="fab fa-instagram"></i></span> </a>
                 </li>
                 <li class="icon youtube">
                     <span class="tooltip">Youtube</span>
-                    <a href="https://www.youtube.com/channel/UCOGukfrt7i9nj6gGWYq6hiQ"><span><i class="fab fa-youtube"></i></span></a>
+                    <a href="https://www.youtube.com/channel/UCOGukfrt7i9nj6gGWYq6hiQ"><span><i class="fab fa-youtube"></i></span> </a>
                 </li>
             </ul>
 
